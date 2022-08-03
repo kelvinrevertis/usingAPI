2a247c8dc166c83425d63d140a2228eadf9f2e8e
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-02 23:03:32 -0300

    Implementado API CEP

---
+++ script.js
@@ -0,0 +1,18 @@
+const cep = document.getElementById('cep')
+const inputForm =(address)=>{
+  document.getElementById('rua').value = address.logradouro
+  document.getElementById('bairro').value = address.bairro
+  document.getElementById('cidade').value = address.localidade
+  document.getElementById('estado').value = address.uf
+
+}
+const searchCep = async()=>{   
+    const valueCep = cep.value
+    const url = `http://viacep.com.br/ws/${valueCep}/json/`
+    const data = await  fetch(url)
+    const address = await data.json()
+    inputForm(address)
+
+}
+
+cep.addEventListener('focusout', searchCep)
